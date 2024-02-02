bba6ad1e361c1045610feaf0a8eee1b556c8492d
Jason Brackman <brackman@gmail.com>
2024-02-02 13:33:09 -0800

    Adding day09 part 2 puzzle solution.

--- day09.py
+++ day09.py
@@ -2,8 +2,10 @@ import re
 
 digit = re.compile(r"-?\d+")
 
+LinesType = list[list[int]]
 
-def parse(path: str) -> list[list[int]]:
+
+def parse(path: str) -> LinesType:
     lines = []
     with open(path, "r", encoding="utf-8") as handle:
         for line in handle.readlines():
@@ -19,37 +21,35 @@ def reduction(nums: list[int]) -> list[list[int]]:
     return history
 
 
-def part01() -> None:
-    lines = parse(r"./data/day09.txt")
-    t = 0
+def part01(lines: LinesType) -> None:
+    total = 0
     for line in lines:
-        h = reduction(line)
-        t += sum(item[-1] for item in h)
-    assert t == 1842168671
+        reduced = reduction(line)
+        total += sum(item[-1] for item in reduced)
+    assert total == 1842168671
+
+
+def part02(lines: LinesType) -> None:
+    total = 0
+    for line in lines:
+        reduced = reduction(line)
+        old = [item[0] for item in reduced]
+        new = []
+        current = 0
+        while old:
+            temp = old.pop()
+            new.append(temp - current)
+            current = temp - current
+
+        total += new[-1]
+
+    assert total == 903
 
 
 def run() -> None:
-    part01()
-
-
-# def new_sub(a: int, b: int) -> int:
-#     return a - b
-#
-#
-# def part02():
-#     lines = parse(r"./data/day09t.txt")
-#     t = 0
-#     for line in lines:
-#         h = reduction(line)
-#         n = [item[0] for item in h][::-1]
-#         print(n)
-#         for a, b in zip(n, n[1:]):
-#             print(a, b, new_sub(a, b))
-#             t += new_sub(a, b)
-#     print(t)
-#
-#
-# part02()
+    lines = parse(r"./data/day09.txt")
+    part01(lines)
+    part02(lines)
 
 
 if __name__ == "__main__":
