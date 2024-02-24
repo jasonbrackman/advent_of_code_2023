3c775368aa28f1473556b305177cdaef3e2f8e98
jasonbrackman <brackman@gmail.com>
2024-02-23 20:46:36 -0800

    Day 18 part 1 completed

--- day18.py
+++ day18.py
@@ -1,13 +1,13 @@
 def parse(file: str) -> list[tuple[tuple[int, int], int, str]]:
     """Parse the file and return a list of strings."""
     dirs = {
-        'U': (-1, 0),
-        'L': (0, 1),
-        'D': (1, 0),
-        'R': (0, -1),
+        "U": (-1, 0),
+        "L": (0, 1),
+        "D": (1, 0),
+        "R": (0, -1),
     }
 
-    with (open(file, 'r', encoding='utf8') as f):
+    with open(file, "r", encoding="utf8") as f:
         data = []
         for line in f.read().splitlines():
             parts = line.split()
@@ -15,7 +15,10 @@ def parse(file: str) -> list[tuple[tuple[int, int], int, str]]:
 
     return data
 
-def create_grid(instructions: list[tuple[tuple[int, int], int, str]]) -> dict[tuple[int, int], str]:
+
+def create_grid(
+    instructions: list[tuple[tuple[int, int], int, str]]
+) -> dict[tuple[int, int], str]:
     grid = {}
 
     current = (0, 0)
@@ -28,11 +31,10 @@ def create_grid(instructions: list[tuple[tuple[int, int], int, str]]) -> dict[tu
     return grid
 
 
-def part01(s: str) -> None:
-
+def part01(s: str) -> int:
     data = parse(s)
     grid = create_grid(data)
-    pprint(grid)
+    # pprint(grid)
 
     t = 0
     min_y = min(grid)[0]
@@ -44,27 +46,53 @@ def part01(s: str) -> None:
         scoop = 0
         for x in range(min_x, max_x + 1):
             if (y, x) not in grid:
-                # how many edges to the right? odd is within an area 0 or even is outside
+                # how many edges to the right? within = odd; outside = 0 or even
                 edge_count = 0
-                found = False
+                y_pos = None
                 for i in range(x, max_x + 1):
-                    # edges should only be counted once (if an edge exits its another edge)
-                    # for example:
-                    #     .    ....   <- 3 edges
-                    #     .    .  .   <- 3 edges
+                    # Count each edge only once (if connected; it may be another edge)
+                    # Example:
+                    #                .                     .   .
+                    #   One Edge ->  . . .   Two Edges ->  . . .
+                    #                    .
 
                     if (y, i) in grid:
-                        if (y, i-1) not in grid or ((i+1) <= max_x and (y, i+1) not in grid):
+                        a = (y, i - 1) not in grid
+                        b = (i + 1) <= max_x and (y, i + 1) not in grid
+                        if a and b:
                             edge_count += 1
+                        elif a:
+                            assert y_pos is None
+                            # left is clear so this is the first occupied space
+                            edge_count += 1
+                            # check up and down
+                            if (y - 1, i) in grid:
+                                y_pos = y - 1
+                            if (y + 1, i) in grid:
+                                y_pos = y + 1
+                        elif b:
+                            assert y_pos is not None
+                            # edge_count += 1
+                            if (y - 1, i) in grid and y_pos == y - 1:
+                                edge_count += 1
+                            elif (y + 1, i) in grid and y_pos == y + 1:
+                                edge_count += 1
+
+                            # else nothing to do but to clear the flag
+                            y_pos = None
 
                 if edge_count % 2 != 0:
                     scoop += 1
                     t += 1
-                # print(edge_count, end='')
+
+                # if edge_count % 2 != 0:
+                #    print(edge_count, end=", ")
+
             else:
                 t += 1
-        print(' ', scoop)
-    return t  # 57222 too high
+        # print(" ", scoop)
+    return t
+
 
 def pprint(grid):
     min_y = min(grid)[0]
@@ -74,17 +102,19 @@ def pprint(grid):
     for y in range(min_y, max_y + 1):
         for x in range(min_x, max_x + 1):
             if (y, x) in grid:
-                print("#", end='')
+                print("#", end="")
             else:
-                print('.', end='')
+                print(".", end="")
             # print(grid.get((y, x), '.'), end='')
         print()
 
 
 def run():
-    t = part01(r'./data/day18t.txt')
+    t = part01(r"./data/day18t.txt")
     assert t == 62
-    p1 = part01(r'./data/day18.txt')
-    print(p1)
+    p1 = part01(r"./data/day18.txt")
+    assert p1 == 50603
+
+
 if __name__ == "__main__":
     run()
